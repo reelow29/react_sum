cd1c4f31848eae234046eb8e6b23e4437510376f
Roma <rasevychr@gmail.com>
2024-10-12 00:25:17 +0300

    sol

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,11 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export function Sum({ a = 0, b = 0 }) {
+  const sum = a + b;
+
+  return (
+    <p>
+      Sum {a} and {b} is {sum}.
+    </p>
+  );
+}
